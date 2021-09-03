e7c79dc9adc07028141bb6fcfe8882689d6b6f77
trif0lium <gh@fleeingsunlight.dev>
2021-09-03 15:33:19 +0700

    feat(iam): Resource + Role + Capability

--- nx/libs/iam/src/lib/iam.ts
+++ nx/libs/iam/src/lib/iam.ts
@@ -1,3 +1,27 @@
-export function iam(): string {
-  return 'iam';
+export class Resource {
+  name: string;
+  roles: Role[];
+
+  constructor(name: string, roles: Role[]) {
+    this.name = name;
+    this.roles = roles;
+  }
+}
+
+export class Role {
+  name: string;
+  description?: string;
+  capabilities: Capability[];
+  constructor(name: string, capabilities: Capability[], description?: string) {
+    this.name = name;
+    this.capabilities = capabilities;
+    this.description = description;
+  }
+}
+
+export class Capability {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
 }
